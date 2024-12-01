e3c58b71945cc68c418495030c3fbed436c94f74
Catuvella <parachorradasuno@gmail.com>
2024-12-01 17:27:24 +0100

    initial setup

---
+++ sketch.js
@@ -0,0 +1,10 @@
+//Credit:training exercise from Colorful Coding, with personal modifications (https://www.youtube.com/watch?v=-3HwUKsovBE&list=PPSV)
+
+function setup(){
+    createCanvas(windowWidth, windowHeight);
+    angleMode(DEGREES);
+}
+
+function draw(){
+    
+}
